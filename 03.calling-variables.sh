3cbcb2c681fc31b59fb15ba05ca2ca29ada237ed
Raghu <kadapalaraghu005@gmail.com>
2025-01-09 21:57:45 +0530

    Shell Script Writing

---
+++ 03.calling-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+HUSBAND=Raghu
+WIFE=Srvani
+
+echo "$HUSBAND ==> Hi $WIFE"
+
+echo "$WIFE ==> Hello $HUSBAND"
+ 
+echo "$HUSBAND ==> How are you $WIFE"
+
+echo "$WIFE ==> I am doing good $HUSBAND"
+
+echo " Thank $HUSBAND and $WIFE for conversation in variable section"
