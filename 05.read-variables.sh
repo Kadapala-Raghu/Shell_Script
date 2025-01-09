ad0945eff30a4fa7e546681f3075d6d014a5c8f3
Raghu <kadapalaraghu005@gmail.com>
2025-01-09 22:09:02 +0530

    Shell Script Writing

---
+++ 05.read-variables.sh
@@ -0,0 +1,11 @@
+#!?bin/bash
+
+echo "Please enter your username:" #
+
+read -s USERNAME # here USERNAME is variable
+
+echo "You have entered username is $USERNAME:"
+
+echo "Enter your password:"
+
+read -s PASSWORD
