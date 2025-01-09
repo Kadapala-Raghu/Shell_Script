2bd8b31d4ac6825ca62f2b88d556ec1b8258d98e
Raghu <kadapalaraghu005@gmail.com>
2025-01-09 22:37:42 +0530

    Shell Script Writing

--- 07.arrays-variables.sh
+++ 07.arrays-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MOVIES=("RRR","ANIMAL","SARDAR")   #Here index starts from zero
+MOVIES=("RRR" "ANIMAL" "SARDAR")   #Here index starts from zero
 
 echo "Our first movie: ${MOVIES[0]}"
 echo "Our first movie: ${MOVIES[1]}"
