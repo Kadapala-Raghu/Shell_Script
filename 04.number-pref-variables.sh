0bbf1b4421153433b6c8e6460805ca234ec285e3
Raghu <kadapalaraghu005@gmail.com>
2025-01-09 22:04:38 +0530

    Shell Script Writing

--- 04.number-pref-variables.sh
+++ 04.number-pref-variables.sh
@@ -5,3 +5,10 @@ PERSON2=$2
 
 echo "$PERSON1 => Hello $2"
 echo "$PERSON2 => Hi $1"
+
+
+## calling [ec2-user@ip-172-31-83-114 Shell_Script]$ sh 04.number-pref-variables.sh  Raghu Sravnai  
+#Raghu => Hello Sravnai
+#Sravnai => Hi Raghu
+# Here Raghu from variable1 value and Sravani from variable2 value
+# varible value at the command prompt it will print order wise which is given first
