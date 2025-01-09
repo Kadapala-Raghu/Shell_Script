4fd24da61167e2012271c987351b62586cb856f6
Raghu <kadapalaraghu005@gmail.com>
2025-01-09 21:42:56 +0530

    Shell Script Writing

---
+++ 02.variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Raghu -> Hello Sravani"
+echo "Srvani -> Hi Hubby, :), WhatsApp"
+echo "Raghu -> I am doing great, thanks for asking. How about you"
+echo "Sravani -> I am doing fantastic :)"
